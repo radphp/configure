e5cfe91960d12eb0aa903030977459a2c7217fb8
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-07-31 15:49:47 +0430

    Change Config Namespace

---
+++ Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rad\Configure;
+
+use Rad\Core\Exception\BaseException;
+
+/**
+ * Config Exception
+ *
+ * @package Rad\Configure
+ */
+class Exception extends BaseException
+{
+
+}
